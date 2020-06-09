e81a57399fa6b8de34012c884f50f1250f27e6b0
Luke Gartland <lukegartland@gmail.com>
2020-06-08 17:33:28 -1000

    first commit

---
+++ random.js
@@ -0,0 +1,105 @@
+
+const arr = ['scroll', 'thank you', 'do no harm', 'connect', 'breathe', 'accept', 'relax', 'be present', 'become aware', 'awaken', 'let it go', 'be happy', 'think positivity', 'think gratitude', 'be thankful', 'center your thoughts', 'forward progress', 'i am strong', 'i am enough', 'i am beautiful', 'transcend', 'reflect'];
+const random = (array) => {
+  for (let i=0; temparray= arr, i < array.length; i++) {
+      const element = Math.floor(Math.random() * temparray.length);
+   //  console.log(
+       return temparray.splice([element], 1);
+       //return array[element];
+  }
+};
+
+let id1 = document.getElementById(1);
+let id2 = document.getElementById(2);
+let id3 = document.getElementById(3);
+let id4 = document.getElementById(4);
+let id5 = document.getElementById(5);
+let id6 = document.getElementById(6);
+let id7 = document.getElementById(7);
+let id8 = document.getElementById(8);
+let id9 = document.getElementById(9);
+let id10 = document.getElementById(10);
+let id11 = document.getElementById(11);
+let id12 = document.getElementById(12);
+let id13 = document.getElementById(13);
+let id14 = document.getElementById(14);
+let id15 = document.getElementById(15);
+let id16 = document.getElementById(16);
+let id17 = document.getElementById(17);
+let id18 = document.getElementById(18);
+let id19 = document.getElementById(19);
+let id20 = document.getElementById(20);
+let id21 = document.getElementById(21);
+let id22 = document.getElementById(22);
+
+id1.innerHTML = `${random(arr)}`;
+id2.innerHTML = `${random(arr)}`;
+id3.innerHTML = `${random(arr)}`;
+id4.innerHTML = `${random(arr)}`;
+id5.innerHTML = `${random(arr)}`;
+id6.innerHTML = `${random(arr)}`;
+id7.innerHTML = `${random(arr)}`;
+id8.innerHTML = `${random(arr)}`;
+id9.innerHTML = `${random(arr)}`;
+id10.innerHTML = `${random(arr)}`;
+id11.innerHTML = `${random(arr)}`;
+id12.innerHTML = `${random(arr)}`;
+id13.innerHTML = `${random(arr)}`;
+id14.innerHTML = `${random(arr)}`;
+id15.innerHTML = `${random(arr)}`;
+id16.innerHTML = `${random(arr)}`;
+id17.innerHTML = `${random(arr)}`;
+id18.innerHTML = `${random(arr)}`;
+id19.innerHTML = `${random(arr)}`;
+id20.innerHTML = `${random(arr)}`;
+id21.innerHTML = `${random(arr)}`;
+id22.innerHTML = `${random(arr)}`;
+
+
+/** **********************
+ * 
+ * 
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(random(arr));
+console.log(`${random(arr)}`);
+
+console.log(arr);
+
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(random(array));
+console.log(`left over array: ${array}`);
+
+*/
